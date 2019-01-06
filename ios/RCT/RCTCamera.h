73702f7631ca601e33c3156f75e2273ae2866502
Vinay Anatharaman <vinayan3@gmail.com>
2019-01-05 16:44:10 -0800

    Remove some things that we aren't using anymore.

--- ios/RCT/RCTCamera.h
+++ ios/RCT/RCTCamera.h
@@ -1,14 +1,10 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 #import "CameraFocusSquare.h"
-#import <React/RCTComponent.h>
 
 @class RCTCameraManager;
 
 @interface RCTCamera : UIView
-
-@property (nonatomic, strong) RCTDirectEventBlock onFocusChanged;
-@property (nonatomic, strong) RCTDirectEventBlock onZoomChanged;
 
 - (id)initWithManager:(RCTCameraManager*)manager bridge:(RCTBridge *)bridge;
 
